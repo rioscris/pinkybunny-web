237c1777dc2ea0a7fdf8a775fa019ccd80748390
rios cristian <cristian.rios@moxit.net>
2021-07-05 03:10:25 -0300

    added chakra ui

--- pinkybunnyweb/src/App.js
+++ pinkybunnyweb/src/App.js
@@ -1,9 +1,25 @@
+import * as React from "react"
+import { ChakraProvider } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react"
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+}
+const theme = extendTheme({  colors })
+
 function App() {
+  // 2. Use at the root of your app
   return (
-    <div>
-      <h2>PinkyBunny web layout</h2>
-    </div>
-  );
+    <ChakraProvider theme={theme}>
+      <div>
+        <h2>PinkyBunny web layout</h2>
+      </div>
+    </ChakraProvider>
+  )
 }
 
 export default App;
